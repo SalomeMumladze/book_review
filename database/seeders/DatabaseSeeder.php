fcffa857204c78b3b3a8ec22595d2f5821f4af9f
salomeMumladze <salomemumladze775@gmail.com>
2023-11-13 11:49:09 +0400

    created fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Book;
+use App\Models\Review;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,6 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\Book::factory(33)->create()->each(function($book){
+            $numberReviews = random_int(5,30);
+            \App\Models\Review::factory()->count($numberReviews)->good()->for($book)->create();
+        });
+        \App\Models\Book::factory(20)->create()->each(function($book){
+            $numberReviews = random_int(5,30);
+            \App\Models\Review::factory()->count($numberReviews)->average()->for($book)->create();
+        });
+        \App\Models\Book::factory(34)->create()->each(function($book){
+            $numberReviews = random_int(5,30);
+            \App\Models\Review::factory()->count($numberReviews)->bad()->for($book)->create();
+        });
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
